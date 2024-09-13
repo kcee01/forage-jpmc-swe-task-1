dcb810cc69f909fde8e15d1090443b8482ffd0d7
Cliff Keabetswe <105539368+kcee01@users.noreply.github.com>
2024-09-13 15:05:21 +0200

    UPDATED THE REQUIRED

--- client3.py
+++ client3.py
@@ -43,7 +43,7 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if(price_b == 0):
-    return 
+        return 
     return price_a/price_b
 
 
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         price = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] = price
+            price[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % getRatio(price["ABC"], price["DEF"]))
